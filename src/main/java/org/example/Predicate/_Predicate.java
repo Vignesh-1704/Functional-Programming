720025b66b4d7fbea04f98eb6f58c388829d3682
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-07-27 15:23:21 +0530

    Add. Added the Additional Chaining Predicate Functions

--- src/main/java/org/example/Predicate/_Predicate.java
+++ src/main/java/org/example/Predicate/_Predicate.java
@@ -11,6 +11,10 @@ public class _Predicate {
         // Result with Predicate Function
         boolean isValidNumber = isValidPhoneNumberOrNot.test("9999988888");
         System.out.println("Given Phone Number is Valid: "+isValidNumber);
+
+        // Result with Double Check
+        boolean isValidPhoneNumber = isValidPhoneNumberDoubleCheck.test("9799988887");
+        System.out.println("Given Phone Number is Valid: "+ isValidPhoneNumber);
     }
     static boolean isPhoneNumberValid(String phoneNumber)
     {
@@ -18,5 +22,9 @@ public class _Predicate {
     }
 
     // Predicate<T,R> Takes a Input and Returns a Boolean
+
     static Predicate<String> isValidPhoneNumberOrNot = phoneNumber -> phoneNumber.startsWith("97") && phoneNumber.length() == 10;
+    static Predicate<String> containsEightInPhoneNumber = phoneNumber -> phoneNumber.contains("8");
+
+    static Predicate<String> isValidPhoneNumberDoubleCheck = isValidPhoneNumberOrNot.and(containsEightInPhoneNumber);
 }
